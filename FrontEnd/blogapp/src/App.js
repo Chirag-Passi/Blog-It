9cfd1bbec788ccb85187ea4d8c71b774885e4ff6
Chirag-Passi <chiragpassi1999@gmail.com>
2023-07-05 19:53:43 +0530

    FrontEnd Routing & Login/Register Page

--- FrontEnd/blogapp/src/App.js
+++ FrontEnd/blogapp/src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom"
 import Layout from './Components/Layout';
 import IndexPage from './Components/IndexPage';
 import LoginPage from './Components/LoginPage';
+import RegisterPage from './Components/RegisterPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Layout />} >
         <Route index element={<IndexPage />} />
         <Route path={'/Components/login'} element={<LoginPage />} />
+        <Route path={'/Components/register'} element={<RegisterPage />} />
       </Route>
     </Routes>
 
