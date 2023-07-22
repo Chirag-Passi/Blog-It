8a95a1e618604629ba521336628c01137f41c694
Chirag-Passi <chiragpassi1999@gmail.com>
2023-07-22 16:03:51 +0530

    Logout Functionality Improved

--- FrontEnd/blogapp/src/App.js
+++ FrontEnd/blogapp/src/App.js
@@ -7,16 +7,19 @@ import Layout from './Components/Layout';
 import IndexPage from './Components/IndexPage';
 import LoginPage from './Components/LoginPage';
 import RegisterPage from './Components/RegisterPage';
+import { UserContextProvider } from './Components/UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<IndexPage />} />
-        <Route path={'/Components/login'} element={<LoginPage />} />
-        <Route path={'/Components/register'} element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<IndexPage />} />
+          <Route path={'/Components/login'} element={<LoginPage />} />
+          <Route path={'/Components/register'} element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
 
   );
 }
--- FrontEnd/blogapp/src/Components/Header.js
+++ FrontEnd/blogapp/src/Components/Header.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
-    const [username, setUsername] = useState(null);
+
+    const { setUserInfo, userInfo } = useContext(UserContext);
     useEffect(() => {
         fetch('http://localhost:4000/profile', {
             credentials: 'include',
         }).then(response => {
             response.json().then(userInfo => {
-                setUsername(userInfo.username);
+                setUserInfo(userInfo);
             });
         });
     }, []);
@@ -17,10 +19,11 @@ export default function Header() {
         fetch('http://localhost:4000/logout', {
             credentials: 'include',
             method: 'POST',
-        })
-        setUsername(null);
+        });
+        setUserInfo(null);
     }
-~
+
+    const username = userInfo?.username;
     return (
         <header>
             <Link to="/" className="logo">MyBlog</Link>
